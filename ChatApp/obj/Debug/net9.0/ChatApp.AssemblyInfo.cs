e4489847f3d1fe10515c261c7bba8ab531647265
Farid <tavakoli127@gmail.com>
2025-02-04 05:49:59 -0800

    version 1.0

--- ChatApp/obj/Debug/net9.0/ChatApp.AssemblyInfo.cs
+++ ChatApp/obj/Debug/net9.0/ChatApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef43bc025c6694771f0feb12ffc5c70c40373404")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3079720306a12f0253e9b9fad43954342c56d63c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
