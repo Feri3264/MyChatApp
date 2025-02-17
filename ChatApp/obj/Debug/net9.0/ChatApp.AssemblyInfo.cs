d69985c81e83732226b39faf43d5cd07b4057a88
Farid <tavakoli127@gmail.com>
2025-02-17 03:49:54 -0800

    releasing version 2.6

--- ChatApp/obj/Debug/net9.0/ChatApp.AssemblyInfo.cs
+++ ChatApp/obj/Debug/net9.0/ChatApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b38ce2b4bc43a7b6b824dbfc9b4b5ad5cdab105")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28a874f23e62466b010e7f5042f683ed5873b1b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
